d30e96785ed14a5197fecf5831122f167793f52f
zhengzhi2936 <zhengzhi2616@gmail.com>
2017-03-10 09:00:30 +0800

    fix bugs

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, "git@github.com:zhengzhi2936/rails-101.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/home/apps/blog'
+set :deploy_to, '/home/apps/rails101'
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
